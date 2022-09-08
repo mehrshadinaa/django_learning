ce392719d7aba07f6e3e3d67957dc22b00b9ecd4
mehrshadinaa <mehrshadinaa@gmail.com>
2022-09-08 21:10:24 +0430

    we are in jadi video2

---
+++ web/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __str__(self):
+        return '{}-token'.format(self.user)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
